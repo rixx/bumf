1908cef518a5c771d27348ef1304cb4acfb7af2a
Tobias Kunze <rixx@cutebit.de>
2017-01-07 23:46:12 +0100

    [core] Add the VirtualAccount model

--- bumf/bumf/core/models/__init__.py
+++ bumf/bumf/core/models/__init__.py
@@ -1,8 +1,12 @@
+from .account import Account, AccountVariantChoices, BankAccount
 from .auth import User
 from .project import Project, ProjectScopeChoices
 
 
 __all__ = [
+    'Account',
+    'AccountVariantChoices',
+    'BankAccount',
     'Project',
     'ProjectScopeChoices',
     'User',
---
+++ bumf/bumf/core/models/account.py
@@ -0,0 +1,63 @@
+from decimal import Decimal
+
+from django.db import models
+
+from bumf.core.models.utils import Choices
+
+
+
+class VirtualAccountVariantChoices(Choices):
+    """
+    There is one main virtual account type, the budget account, used for budget
+    categories. And there are liabilities …
+    There are two virtual account types. Budget accounts for money you have,
+    and liability accounts for money you owe. Then there is your income …
+    There are three virutal account types. Budget accounts and income accounts
+    for money you have or receive, and liabilities for money you owe. And of
+    course your expenses …
+
+    Amongst our chief virtual account types are such things as:
+     - Budget accounts, holding money the user has and wishes to spend on that
+       budget category
+     - Liability accounts for debts and open invoices. For private users this
+       will mostly be one hidden/magic account.
+     - Income accounts, for money the user receives from external sources
+     - Expense accounts for money spent.
+    """
+    BUDGET = 'budget'
+    LIABILITY = 'liability'
+    INCOME = 'income'
+    EXPENSE = 'expense'
+
+    valid_choices = [
+        BUDGET, LIABILITY, INCOME, EXPENSE,
+    ]
+
+
+class VirtualAccount(models.Model):
+    """
+    Virtual Accounts are used to budget money for its intended use. They are
+    the heart of bumf and are roughly equivalent to YNAB's categories.
+    """
+    name = models.CharField(
+        max_length=120,
+        verbose_name='Name',
+    )
+    variant = models.CharField(
+        max_length=VirtualAccountVariantChoices.get_max_length(),
+        default=VirtualAccountVariantChoices.BUDGET,
+        choices=VirtualAccountVariantChoices.get_choices(),
+    )
+    project = models.ForeignKey(
+        to='Project',
+        on_delete=models.PROTECT,
+        related_name='virtual_accounts',
+    )
+    parent = models.ForeignKey(
+        to='VirtualAccount', null=True, blank=True,
+        on_delete=models.PROTECT,
+        related_name='child_accounts',
+    )
+    comment = models.CharField(
+        max_length=1000, null=True, blank=True,
+    )
