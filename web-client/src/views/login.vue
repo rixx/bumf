27ed6bc4f3dbe2db65c010c22cca138b5c95fcf7
Tobias Kunze <rixx@cutebit.de>
2017-01-30 22:56:58 +0100

    [web] Add a registration form

--- web-client/src/views/login.vue
+++ web-client/src/views/login.vue
@@ -1,11 +1,29 @@
 <template>
-  <form id="bumf-login">
-    <bunt-input name="username" label="username" v-model="username" />
-    <bunt-input name="password" label="password" type="password" v-model="password" />
-    <bunt-button id="button-login" @click.native="login">
-      Log in
-    </bunt-button>
-  </form>
+  <div id="bumf">
+    <div id="exposition">
+      <h2>welcome!</h2>
+      bumf is a budgeting tool - it is open source, ready to be self-hosted and
+      is ready to help you keep your finances in order!
+    </div>
+    <div id="form-container">
+      <form class="bumf-userform" id="bumf-register">
+        <bunt-input name="username" label="username" v-model="register.username" />
+        <bunt-input name="password" label="password (highly secret!)" type="password" v-model="register.password" />
+        <bunt-input name="firstname" label="What should we call you? (optional)" v-model="register.firstname" />
+        <bunt-input name="email" label="How can we reach you? (optional)" v-model="register.email" />
+        <bunt-button id="button-register" class="bumf-userbutton" @click.native="performRegister">
+          register
+        </bunt-button>
+      </form>
+      <form class="bumf-userform" id="bumf-login">
+        <bunt-input name="username" label="username" v-model="login.username" />
+        <bunt-input name="password" label="password" type="password" v-model="login.password" />
+        <bunt-button id="button-login" class="bumf-userbutton" @click.native="performLogin">
+          log in
+        </bunt-button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,13 +32,28 @@ import api from 'lib/api'
 export default {
   data () {
     return {
-      username: '',
-      password: ''
+      login: {
+        username: '',
+        password: ''
+      },
+      register: {
+        username: '',
+        password: '',
+        firstname: '',
+        email: ''
+      }
     }
   },
   methods: {
-    login () {
-      api.login(this.username, this.password)
+    performLogin () {
+      api.login(this.login.username, this.login.password).then(() => {
+        this.$router.push({ path: '/' })
+      })
+    },
+    performRegister () {
+        api.register(this.register.username, this.register.password, this.register.firstname, this.register.email).then(() => {
+        this.$router.push({ path: '/' })
+      })
     },
   },
 }
