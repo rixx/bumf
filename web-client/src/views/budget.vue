47175cd3bca8cb449fdcb037d7c719848b17b254
Tobias Kunze <rixx@cutebit.de>
2017-03-18 16:29:26 +0100

    [web] Show active budget details

--- web-client/src/views/budget.vue
+++ web-client/src/views/budget.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="budget" v-if="budgets">
     <div id="budget-header"></div>
-    <table id="budget-table">
-      <thead>
-        <th class="budget-name"></th>
-        <th class="budget-in">Budgeted this month</th>
-        <th class="budget-out">Spent this month</th>
-        <th class="budget-total">Total</th>
-      </thead>
-      <tbody>
-        <tr v-for="budget in budgets" @click="activeBudget=(budget === activeBudget) ? null : budget" :class="{active: budget === activeBudget}">
-          <td class="budget-name">{{ budget.name }}</td>
-          <td class="budget-in"></td>
-          <td class="budget-out"></td>
-          <td class="budget-total">{{ budget.total }} €</td>
-        </tr>
-    </table>
+    <div class="budget-body">
+      <div class="budget-table">
+      <table id="budget-table">
+        <thead>
+          <th class="budget-name"></th>
+          <th class="budget-in">Budgeted this month</th>
+          <th class="budget-out">Spent this month</th>
+          <th class="budget-total">Total</th>
+        </thead>
+        <tbody>
+          <tr v-for="budget in budgets" @click="activeBudget=(budget === activeBudget) ? null : budget" :class="{active: budget === activeBudget}">
+            <td class="budget-name">{{ budget.name }}</td>
+            <td class="budget-in"></td>
+            <td class="budget-out"></td>
+            <td class="budget-total">{{ budget.total }} €</td>
+          </tr>
+      </table>
+      </div>
+      <div class="budget-detail" v-if="activeBudget">
+        {{ activeBudget }}
+        <bunt-button>blubb</bunt-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -51,10 +59,23 @@ export default {
 .budget
   width: 100%
 
+.budget-body
+  display: flex
+  flex-direction: row
+  flex-wrap: nowrap
+
+.budget-detail
+  width: 320px
+  flex-grow: 0
+
 #budget-header
   width: 100%
   background-color: $clr-cyan-700
   padding: 8px
+
+.budget-table
+  flex-shrink: 1
+  flex-grow: 1
 
 #budget-table
   margin: 0
