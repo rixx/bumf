97f34d8d97f9da756944603850a2e15f26af62a8
Tobias Kunze <rixx@cutebit.de>
2017-03-18 16:01:48 +0100

    [web] Put things in a table

--- web-client/src/views/budget.vue
+++ web-client/src/views/budget.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="budget" v-if="budgets">
-    {{ budgets }}
+    <div id="budget-header"></div>
+    <table id="budget-table">
+      <thead>
+        <th></th>
+        <th>Budgeted this month</th>
+        <th>Spent this month</th>
+        <th>Total</th>
+      </thead>
+      <tbody>
+        <tr v-for="budget in budgets">
+          <td>{{ budget.name }}</td>
+          <td></td>
+          <td></td>
+          <td>{{ budget.total }}</td>
+        </tr>
+    </table>
   </div>
 </template>
 
